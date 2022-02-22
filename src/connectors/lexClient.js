4f4eda96809d1c1d0a57ef6efd4cc5c037d2ee28
Vikas <mvikasraj123@gmail.com>
2022-02-22 14:36:20 -0600

    Aws amplify (#3)
    * Added intial AWS Configuration
    
    * Add reducers to the miles
    
    * Connectors, Fotter and button responsive, eslinting
    
    * remove padding for message layout
    
    * Bot response along with user entered query storing as thread in store
    
    * LexReponse to LexThread
    
    * Added changes for user messages and bot messages
    
    * Added language screen integration
    
    * Added try catch for logging errors
    
    Co-authored-by: “vikas-dmv” <“vikasraj.mirdoodi@dmv.ca.gov”>
    Co-authored-by: sunil-dmv <suni.biradar@dmv.ca.gov>
    Co-authored-by: sunilb0575 <sunilb0575@gmail.com>

--- src/connectors/lexClient.js
+++ src/connectors/lexClient.js
@@ -2,23 +2,26 @@ import { Interactions } from 'aws-amplify'
 import { lexPostCall, setSearchTerm } from '../ducks/lexClient'
 
 export const leXTextCall = searchTerm => async (dispatch, getState) => {
-  const { lexThread } = getState().lexClient
-  const response = await Interactions.send(
-    process.env.REACT_APP_MILES_BOT,
-    searchTerm
-  )
-  const newThread = [
-    ...lexThread,
-    {
-      message: response.message,
-      buttons: response?.responseCard?.genericAttachments[0]?.buttons
-        ? response?.responseCard?.genericAttachments[0]?.buttons
-        : [],
-      type: 'bot',
-    },
-  ]
-
-  dispatch(lexPostCall(newThread))
+  try {
+    const { lexThread } = getState().lexClient
+    const response = await Interactions.send(
+      process.env.REACT_APP_MILES_BOT,
+      searchTerm
+    )
+    const newThread = [
+      ...lexThread,
+      {
+        message: response.message,
+        buttons: response?.responseCard?.genericAttachments[0]?.buttons
+          ? response?.responseCard?.genericAttachments[0]?.buttons
+          : [],
+        type: 'bot',
+      },
+    ]
+    dispatch(lexPostCall(newThread))
+  } catch(err){
+   console.log(err)
+  }
 }
 
 export const searchQuery = query => (dispatch, getState) => {
