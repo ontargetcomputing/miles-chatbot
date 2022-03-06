ace52b1ea2bb1caf46300ac2fed80041b1c85ff7
Richard Bach <balutbomber@gmail.com>
2022-03-05 18:37:24 -0800

    QBPM-9 - Adding CI/CD (#9)
    Added on_pr.yml and label_check.yml to run on pull requests.  Also updated README.md

--- src/index.js
+++ src/index.js
@@ -4,22 +4,25 @@ import App from "./App"
 import Amplify from "aws-amplify"
 import { Provider } from "react-redux"
 import store from "./store"
-
-Amplify.configure({
+     
+const amplifyConfig = {
   Auth: {
     identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
     region: process.env.REACT_APP_AWS_REGION,
   },
   Interactions: {
-    bots: {
-      miles_dev_dev_dev_master_one_BotKeXvR: {
+    bots: {},
+  },
+}
+
+amplifyConfig.Interactions.bots[process.env.REACT_APP_MILES_BOT] = {
         name: process.env.REACT_APP_MILES_BOT,
         alias: process.env.REACT_APP_ALIAS,
         region: process.env.REACT_APP_AWS_REGION,
-      },
-    },
-  },
-})
+      }
+      
+
+Amplify.configure(amplifyConfig)
 
 ReactDOM.render(
   <React.StrictMode>
