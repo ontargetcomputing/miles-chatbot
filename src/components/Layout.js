3a9dae3dcd08d35be146ddeb4eda4c275093f3f7
“vikas-dmv” <vikasraj.mirdoodi@dmv.ca.gov>
2022-05-25 12:30:56 -0500

    fixed buttons when no agent available

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ import ChatMessagesLayout from "./ChatMessagesLayout"
 import { leXTextCall } from "../connectors/lexClient"
 import IdleTime from "./Idle"
 import { Util } from "../helper/Util"
-import { agentAvailable, resetIdleTimer } from "../ducks/lexClient"
+import { setAgentAvailable, resetIdleTimer } from "../ducks/lexClient"
 import { Loading } from '@ca-dmv/core';
 
 const LayoutWrapper = styled.div`
@@ -25,27 +25,28 @@ const LayoutWrapper = styled.div`
 
 function Layout() {
   const dispatch = useDispatch()
-  const { actionType, lexThread, chatEnded, searchTerm, isLoading, isAgentTyping } = useSelector((store) => store.lexClient)
+  const { actionType, lexThread, chatEnded, searchTerm, isLoading, isAgentTyping, agentAvailable } = useSelector((store) => store.lexClient)
   const { isChatEnded } = chatEnded
   const checkActionType = actionType === ACTION_TYPE.LANGUAGES
   const isVisibleChatInput = !checkActionType && !isChatEnded
   const isShowLoading=isLoading||isAgentTyping;
+  let isAgentAvailable=agentAvailable;
   useEffect(() => {
     const topicChanged = Util.getPropsFromArray(LEXTHREAD_PROPS.TOPIC, lexThread) === TOPIC.LANGUAGE_CHANGED
-    const liveChatTopic = Util.getPropsFromArray(LEXTHREAD_PROPS.TOPIC, lexThread) === 'liveChatStatus.starting'
+    const recentThread = lexThread.length && lexThread[lexThread.length - 1]
+    isAgentAvailable = recentThread?.isAgentAvailable //false
 
     if (topicChanged) {
       dispatch(leXTextCall(searchTerm))
-    } else if (liveChatTopic) {
-      dispatch(agentAvailable(true))
     }
+    dispatch(setAgentAvailable(isAgentAvailable));
     dispatch(resetIdleTimer());
   }, [lexThread])
   return (
     <>
       <IdleTime idleTimeInSeconds={600} warnTimeInSeconds={60} />
       <LayoutWrapper className={isLoading ? "cb-disabled" : ""}>
-        {checkActionType ? <ChangeLanguage /> : <ChatMessagesLayout />}
+        {checkActionType ? <ChangeLanguage /> : <ChatMessagesLayout agentAvailable={isAgentAvailable}/>}
         {isShowLoading&& <Loading loadingText={isAgentTyping?"Agent is typing...": "Loading..."}/>}
       </LayoutWrapper>
 
